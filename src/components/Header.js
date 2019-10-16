a12ef44eb7ccf22489ef7373d0e930f3c0787ede
Shahla Atiq <shahlatiq24@gmail.com>
2019-10-16 12:11:34 +0100

    Learning components

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+
+    render(){
+        return (
+            <h1>My ToDo List</h1>
+
+        );
+
+    }
+
+
+}
+
+export default Header;
---
+++ src/components/Intro.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Intro extends React.Component {
+
+    render(){
+        return (
+            <p>Welcome to my new app</p>
+
+        );
+
+    }
+
+
+}
+
+export default Intro;
